b70cc5e1683bec6984d24375181644519fafd351
Floy3D <floy3d@theawarenessday.com>
2024-11-07 12:32:13 +0200

    first commit

---
+++ src/main/java/com/testtasks/hacken/transactions_gateway/TransactionsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.testtasks.hacken.transactions_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionsGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testtasks/hacken/transactions_gateway/TransactionsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testtasks.hacken.transactions_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
